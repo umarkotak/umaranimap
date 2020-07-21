1e9ea70b21ba84509eb6b5805142557d170b0bab
umar.ramadhana <umar.ramadhana@gmail.com>
2020-07-21 21:18:11 +0700

    enable pwa

--- src/components/PageHome.js
+++ src/components/PageHome.js
@@ -5,9 +5,9 @@ function PageHome() {
     <div>
       <div className="jumbotron p-3 p-md-5 text-white rounded bg-dark">
         <div className="col-md-6 px-0">
-          <h1 className="display-4 font-italic text-dark">ANIMAPU</h1>
-          <p className="lead my-3 text-dark font-weight-bold">Welcome to animapu! This website is dedicated for learning purpose</p>
-          <p className="lead mb-0"><a href="/" class="text-dark font-weight-bold">Hello There Have a Good Day!</a></p>
+          <h1 className="display-4 font-italic text-light">ANIMAPU</h1>
+          <p className="lead my-3 text-light font-weight-bold">Welcome to animapu! This website is dedicated for learning purpose</p>
+          <p className="lead mb-0"><a href="/" class="text-light font-weight-bold">Hello There Have a Good Day!</a></p>
         </div>
       </div>
     </div>
