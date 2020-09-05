304bf05a04af682e75b2a5638f8f758b94dc4ee0
umar.ramadhana <umar.ramadhana@gmail.com>
2020-09-05 15:55:32 +0700

    modify home and clean up navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,26 +13,31 @@ function Nav() {
             <Link to="/" className="blog-header-logo text-dark">ANIMAPU</Link>
           </div> */}
           <div className="col-4 d-flex justify-content-end align-items-center">
-            <Link to="/" className="btn btn-sm btn-outline-secondary">Hello World!</Link>
+            <Link to="/" className="btn btn-sm btn-outline-secondary mx-2">
+              Login
+            </Link>
+            <Link to="/" className="btn btn-sm btn-danger mx-2">
+              Logout
+            </Link>
           </div>
         </div>
       </header>
       <div className="nav-scroller py-1 mb-2">
         <nav className="nav d-flex justify-content-between">
           <Link to="/" className="p-2 m-1 text-muted">Home</Link>
-          <Link to="/tic-tac-toe" className="p-2 m-1 text-muted">Tic Tac Toe</Link>
+          {/* <Link to="/tic-tac-toe" className="p-2 m-1 text-muted">Tic Tac Toe</Link> */}
           {/* <Link to="/learn-react" className="p-2 m-1 text-muted">Learn React</Link>
           <Link to="/airing-anime-v1" className="p-2 m-1 text-muted">Airing Anime V1</Link>
           <Link to="/airing-anime-v2" className="p-2 m-1 text-muted">Airing Anime V2</Link>
           <Link to="/airing-anime-v3" className="p-2 m-1 text-muted">Airing Anime V3</Link>
           <Link to="/airing-anime-v4" className="p-2 m-1 text-muted">Airing Anime V4</Link>
           <Link to="/airing-anime-v5" className="p-2 m-1 text-muted">Airing Anime V5</Link> */}
-          <Link to="/airing-anime-v5" className="p-2 m-1 text-muted">Anime List</Link>
+          <Link to="/airing-anime-v5" className="p-2 m-1 text-muted">Seasonal Anime</Link>
           {/* <Link to="/read-manga-v1" className="p-2 m-1 text-muted">Read Manga</Link> */}
           {/* <Link to="/read-manga-v2" className="p-2 m-1 text-muted">Read Manga</Link> */}
           {/* <Link to="/read-manga-v3" className="p-2 m-1 text-muted">Read Manga</Link> */}
           <Link to="/read-manga-v4" className="p-2 m-1 text-muted">Read Manga</Link>
-          <Link to="/watch-anime-v1" className="p-2 m-1 text-muted">Watch Anime</Link>
+          {/* <Link to="/watch-anime-v1" className="p-2 m-1 text-muted">Watch Anime</Link> */}
         </nav>
       </div>
     </div>
--- src/components/PageHome.js
+++ src/components/PageHome.js
@@ -1,13 +1,28 @@
 import React from "react"
+import {Link} from "react-router-dom"
 
 function PageHome() {
   return (
     <div>
-      <div className="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+      {/* <img src={window.location.origin + '/dashboard.png'} style={{width: "100%"}} /> */}
+      <div className="jumbotron p-3 p-md-5 border rounded text-dark" style={{backgroundImage: `url(${window.location.origin + '/dashboard.png'})`, backgroundSize: 'cover'}}>
         <div className="col-md-6 px-0">
-          <h1 className="display-4 font-italic text-light">ANIMAPU</h1>
-          <p className="lead my-3 text-light font-weight-bold">Welcome to animapu! This website is dedicated for learning purpose</p>
-          <p className="lead mb-0"><a href="/" className="text-light font-weight-bold">Hello There Have a Good Day!</a></p>
+          <h1 className="display-4 font-italic text-primary">ANIMAPU</h1>
+          <p className="lead my-3 text-primary">Welcome to animapu! This website is dedicated for learning purpose</p>
+          <p className="lead mb-0"><a href="/" className="font-weight-bold text-info">Hello There Have a Good Day!</a></p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12">
+          <div className="card">
+            <div class="card-header">
+              Other Pages
+            </div>
+            <div class="card-body">
+              <Link to="/tic-tac-toe" className="btn btn-block btn-primary">Tic Tac Toe</Link>
+              <Link to="/watch-anime-v1" className="btn btn-block btn-primary">Watch Anime</Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
