538bde4d67362e37466d653533dfeebf2ebc425c
umar.ramadhana <umar.ramadhana@gmail.com>
2020-12-27 07:49:17 +0700

    create global clip feature preview 2

--- src/components/PageGlobalClipboardV1.js
+++ src/components/PageGlobalClipboardV1.js
@@ -16,17 +16,39 @@ function PageGlobalClipboardV1() {
       const results = await response.json()
       var temp_clips = new Map(Object.entries(results))
       console.log(temp_clips)
-      var i = []
-      var x = temp_clips.forEach((val, key) => i.push(key))
-      console.log(i)
+      var temp_clip_keys = []
+      temp_clips.forEach((val, key) => temp_clip_keys.push(key))
+      temp_clip_keys.reverse()
+      console.log(temp_clip_keys)
       set_clips(temp_clips)
-      set_clip_keys(i)
+      set_clip_keys(temp_clip_keys)
     }
     fetchTodayMangaData()
   }, [])
 
-  function sendClip() {
+  async function sendClip() {
+    try {
+      const response = await fetch('http://go-animapu.herokuapp.com/clips', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          content: message
+        })
+      })
+      const results = await response.json()
+      const status = await response.status
 
+      if (status === 200) {
+        window.location.reload();
+      } else {
+        alert(results.message);
+      }
+
+    } catch (e) {
+      alert(e.message);
+    }
   }
 
   return (
@@ -36,27 +58,32 @@ function PageGlobalClipboardV1() {
         </div>
 
         <div className="row">
-          <div className="col-12 col-sm-9">
+          <div className="col-12 col-sm-9 col-md-10">
             <div className="form-group">
-              <label>Message</label>
+              <label>Content</label>
               <input className="form-control my-1" type="text" value={message} onChange={(e) => set_message(e.target.value)} />
             </div>
           </div>
-          <div className="col-12 col-sm-3">
+          <div className="col-12 col-sm-3 col-md-2">
             <div className="form-group">
-              <label>.</label>
-              <button className="btn btn-success btn-md btn-block my-1" onClick={sendClip}>send message</button>
+              <label>_</label>
+              <button className="btn btn-success btn-md btn-block my-1" onClick={sendClip}>send content</button>
             </div>
           </div>
         </div>
 
         <div className="row">
           {clip_keys.map(clip_key => (
-            <div className="col-12 my-1" key={clip_key}>
-              <div className="overflow-auto py-2 px-3 border border-secondary shadow" style={{maxHeight: "250px", textAlign: "justify", background: "#ffffff"}}>
-                <code>
-                  {clips.get(clip_key).content}
-                </code>
+            <div className="col-12 my-2" key={clip_key}>
+              <div className="container border border-secondary shadow py-1 rounded">
+                <label><b>code : </b> {clip_key}</label>
+                <button className="btn btn-danger btn-sm float-right">delete</button>
+
+                <div className="overflow-auto py-1 border-top border-success" style={{maxHeight: "250px", textAlign: "justify", background: "#ffffff"}}>
+                  <code>
+                    {clips.get(clip_key).content}
+                  </code>
+                </div>
               </div>
             </div>
           ))}
