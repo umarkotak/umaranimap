d5aecb97624173fd454885d947775b2de2551b3f
umar.ramadhana <umar.ramadhana@gmail.com>
2020-12-29 07:26:52 +0700

    testing local go-animapu

--- src/components/PageReadMangaV8.js
+++ src/components/PageReadMangaV8.js
@@ -7,6 +7,7 @@ import {WhatsappShareButton} from "react-share";
 const cookies = new Cookies();
 var cdn_host = "https://img.mghubcdn.com/file/imghub"
 var go_animapu_host = "http://go-animapu.herokuapp.com"
+// var go_animapu_host = "http://localhost:3005"
 var animapu_host = "http://animapu.herokuapp.com"
 
 var qs = require('qs');
