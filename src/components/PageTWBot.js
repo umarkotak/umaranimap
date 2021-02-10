441b891b68fc90935d7c5569187d7ce9b0985c10
umar.ramadhana <umar.ramadhana@gmail.com>
2021-02-10 21:23:59 +0700

    fix ts check

--- src/components/PageTWBot.js
+++ src/components/PageTWBot.js
@@ -453,7 +453,9 @@ function PageTWBot() {
           throw "FOUND";
         } else {
           var unixTimeNow = + new Date()
+          unixTimeNow = Math.floor(unixTimeNow / 1000)
 
+          console.log("VS TIME", element.time_completed, unixTimeNow)
           if (element.time_completed < unixTimeNow) {
             handleFinishJob(element.id)
           }
