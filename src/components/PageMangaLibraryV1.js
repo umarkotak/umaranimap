53b0947255999ed0162cef6235efe75763905965
umar.ramadhana <umar.ramadhana@gmail.com>
2021-03-21 05:49:26 +0700

    remove auto reload

--- src/components/PageMangaLibraryV1.js
+++ src/components/PageMangaLibraryV1.js
@@ -284,7 +284,7 @@ function PageMangaLibraryV1() {
 
       if (status === 200) {
         alert("manga successfully removed from library!")
-        window.location.reload(false)
+        // window.location.reload(false)
       } else {
         alert(results.message);
       }
