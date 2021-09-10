c415b36fac17a7502a0c9bcdcb2a7e97c5364871
umarkotak <umarkotak@gmail.com>
2021-09-10 23:41:50 +0700

    add version

--- src/components/pages/PageHome.js
+++ src/components/pages/PageHome.js
@@ -21,7 +21,7 @@ function PageHome() {
 
       <footer className="main-footer bg-dark">
         <div className="float-right">
-          ANIMAPU 2021 | Version: 2.1
+          ANIMAPU 2021 | Version: 2.2
         </div>
       </footer>
     </div>
