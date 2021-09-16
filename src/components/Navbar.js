536a711810b5234f44378350c0b75a7083301ffa
umar.ramadhana <umar.ramadhana@gmail.com>
2021-09-16 16:36:51 +0700

    new navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,6 +10,8 @@ function Navbar() {
       localStorage.setItem("ANIMAPU_ACTIVE_MANGA_SOURCE", "maid_my")
     } else if (source === "mangahub") {
       localStorage.setItem("ANIMAPU_ACTIVE_MANGA_SOURCE", "mangahub")
+    } else if (source === "klik_manga") {
+      localStorage.setItem("ANIMAPU_ACTIVE_MANGA_SOURCE", "klik_manga")
     } else if (source === "mangadex") {
       localStorage.setItem("ANIMAPU_ACTIVE_MANGA_SOURCE", "mangadex")
       localStorage.setItem("ANIMAPU_MANGADEX_LANG", "en")
@@ -97,10 +99,11 @@ function Navbar() {
         onChange={(e) => handleChangeSource(e.target.value)}
         defaultValue={mangaSourceDefaultValueDecider()}
       >
-        <option key="mangadex" value="mangadex"> (EN) mangadex </option>
-        <option key="mangahub" value="mangahub"> (EN) mangahub </option>
-        <option key="mangadex_id" value="mangadex_id"> (ID) mangadex </option>
-        <option key="maid_my" value="maid_my"> (ID) maid_my </option>
+        <option key="mangadex" value="mangadex"> (EN) MangaDex </option>
+        <option key="mangahub" value="mangahub"> (EN) MangaHub </option>
+        <option key="mangadex_id" value="mangadex_id"> (ID) MangaDex </option>
+        <option key="maid_my" value="maid_my"> (ID) Maid My </option>
+        <option key="klik_manga" value="klik_manga"> (ID) Klik Manga </option>
       </select>
     )
   }
