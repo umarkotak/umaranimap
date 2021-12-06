7c111088b6b1642d8ca6d00380b83b9c7b4de08e
umar.ramadhana <umar.ramadhana@gmail.com>
2021-12-06 10:00:24 +0700

    move form

--- src/components/pages/PageLogin.js
+++ src/components/pages/PageLogin.js
@@ -38,12 +38,12 @@ function PageLogin() {
       <div className="content-wrapper" style={{backgroundColor: "#454d55"}}>
         <div className="wrapper" >
           <div className="container pt-4" >
-            <form onSubmit={handleLoginSubmit}>
-              <div className="card border-success tab-pane active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                <div className="card-header">
-                  Login
-                </div>
-                <div className="card-body">
+            <div className="card border-success tab-pane active" id="home" role="tabpanel" aria-labelledby="home-tab">
+              <div className="card-header">
+                Login
+              </div>
+              <div className="card-body">
+                <form onSubmit={handleLoginSubmit}>
                   <div className="form-group">
                     <label>Username</label>
                     <input type="text" className="form-control" required autoComplete="off" onChange={e => set_username(e.target.value)} />
@@ -54,70 +54,70 @@ function PageLogin() {
                   </div>
                   <hr/>
                   <button className="btn btn-block btn-primary">Login</button>
-                  <hr/>
-                  <div>
-                    <GoogleLogin
-                      clientId="334886517586-djci4jil803sqjk042f6nne3016bngni.apps.googleusercontent.com"
-                      buttonText="Login"
-                      onSuccess={handleGoogleCallback}
-                      onFailure={handleGoogleCallback}
-                      cookiePolicy={'single_host_origin'}
-                      render={renderProps => (
-                        <button className="btn btn-block btn-light" onClick={renderProps.onClick} disabled={renderProps.disabled}>
-                          <i className="fab fa-google text-primary"></i> Login With Google
-                        </button>
-                      )}
-                    />
-                    <GoogleLogout
-                      clientId="334886517586-djci4jil803sqjk042f6nne3016bngni.apps.googleusercontent.com"
-                      buttonText="Logout"
-                      onLogoutSuccess={handleGoogleLogoutCallback}
-                      onFailure={handleGoogleLogoutCallback}
-                      render={renderProps => (
-                        <button className="btn btn-block btn-light" onClick={renderProps.onClick} disabled={renderProps.disabled}>
-                          <i className="fab fa-google text-danger"></i> Logout From Google
-                        </button>
-                      )}
-                    />
-                    <FacebookLogin
-                      appId="1050889079041740"
-                      autoLoad={true}
-                      fields="name,email,picture"
-                      scope="public_profile,email"
-                      callback={responseFacebook}
-                      render={renderProps => (
-                        <button className="btn btn-block btn-light" onClick={renderProps.onClick}>
-                          <i className="fab fa-facebook text-primary"></i> Login With FaceBook
-                        </button>
-                      )}
-                    />
-                    {/* <div id="appleid-signin" data-color="black" data-border="true" data-type="sign in"></div> */}
-                    <AppleLogin
-                      clientId="goplay.apple.login.test"
-                      redirectURI="https://animapu.netlify.app/goplay/apple/account/return"
-                      scope="name%20email"
-                      responseType="code%20id_token"
-                      responseMode="form_post"
-                      render={renderProps => (
-                        <button className="btn btn-block btn-light" onClick={renderProps.onClick}>
-                          <i className="fab fa-apple text-primary"></i> Login With Apple
-                        </button>
-                      )}
-                    />
-                    <TwitterLogin
-                      authCallback={responseTwitter}
-                      consumerKey={"sKg9AyClMaDWw56uo5eBt57Qk"}
-                      consumerSecret={"KZosfxHqFXijiZgFv4WaypxosSruO9zE4333T2Jqu5pzW5G20w"}
-                      children={
-                        <button className="btn btn-block btn-light mt-2">
-                          <i className="fab fa-twitter text-primary"></i> Login With Twitter
-                        </button>
-                      }
-                    />
-                  </div>
+                </form>
+                <hr/>
+                <div>
+                  <GoogleLogin
+                    clientId="334886517586-djci4jil803sqjk042f6nne3016bngni.apps.googleusercontent.com"
+                    buttonText="Login"
+                    onSuccess={handleGoogleCallback}
+                    onFailure={handleGoogleCallback}
+                    cookiePolicy={'single_host_origin'}
+                    render={renderProps => (
+                      <button className="btn btn-block btn-light" onClick={renderProps.onClick} disabled={renderProps.disabled}>
+                        <i className="fab fa-google text-primary"></i> Login With Google
+                      </button>
+                    )}
+                  />
+                  <GoogleLogout
+                    clientId="334886517586-djci4jil803sqjk042f6nne3016bngni.apps.googleusercontent.com"
+                    buttonText="Logout"
+                    onLogoutSuccess={handleGoogleLogoutCallback}
+                    onFailure={handleGoogleLogoutCallback}
+                    render={renderProps => (
+                      <button className="btn btn-block btn-light" onClick={renderProps.onClick} disabled={renderProps.disabled}>
+                        <i className="fab fa-google text-danger"></i> Logout From Google
+                      </button>
+                    )}
+                  />
+                  <FacebookLogin
+                    appId="1050889079041740"
+                    autoLoad={true}
+                    fields="name,email,picture"
+                    scope="public_profile,email"
+                    callback={responseFacebook}
+                    render={renderProps => (
+                      <button className="btn btn-block btn-light" onClick={renderProps.onClick}>
+                        <i className="fab fa-facebook text-primary"></i> Login With FaceBook
+                      </button>
+                    )}
+                  />
+                  {/* <div id="appleid-signin" data-color="black" data-border="true" data-type="sign in"></div> */}
+                  <AppleLogin
+                    clientId="goplay.apple.login.test"
+                    redirectURI="https://animapu.netlify.app/goplay/apple/account/return"
+                    scope="name%20email"
+                    responseType="code%20id_token"
+                    responseMode="form_post"
+                    render={renderProps => (
+                      <button className="btn btn-block btn-light" onClick={renderProps.onClick}>
+                        <i className="fab fa-apple text-primary"></i> Login With Apple
+                      </button>
+                    )}
+                  />
+                  <TwitterLogin
+                    authCallback={responseTwitter}
+                    consumerKey={"sKg9AyClMaDWw56uo5eBt57Qk"}
+                    consumerSecret={"KZosfxHqFXijiZgFv4WaypxosSruO9zE4333T2Jqu5pzW5G20w"}
+                    children={
+                      <button className="btn btn-block btn-light mt-2">
+                        <i className="fab fa-twitter text-primary"></i> Login With Twitter
+                      </button>
+                    }
+                  />
                 </div>
               </div>
-            </form>
+            </div>
           </div>
         </div>
       </div>
